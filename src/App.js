a617c125e581faf59e3e113a61a0cb8399d96d05
pramilaniroshan <pramilaniroshan@gmail.com>
2022-07-20 23:23:25 +0530

    Tailwind test class added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './App.css'
+
 const App = () => {
   return (
-    <div>React Dashboard</div>
+    <p className="text-2xl font-bold underline">React Dashboard</p>
   )
 }
 
